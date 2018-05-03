ec3d087e29e7f13a0cbe37eedde903b834e5520b
Francesco Bianco <info.francescobianco@gmail.com>
2018-05-03 16:04:20 +0200

    Deploy as gio  3 mag 2018, 16.04.20, CEST

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -eux
 
 git commit -am "Deploy as $(date)" && git push
 
-IMAGE_NAME=javanile/adminer
+IMAGE=javanile/adminer
 
 docker build --no-cache --pull -t $IMAGE:latest .
 docker push $IMAGE:latest
