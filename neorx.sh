6b23bbef12ca5a018caf376acdb4ac8435fb13f9
Francesco Bianco <info.francescobianco@gmail.com>
2019-12-13 16:52:20 +0100

    Update neorx.sh

--- neorx.sh
+++ neorx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export NEORX_LABEL=adminer
+export NEORX_LABEL="adminer"
 set -x
 
-case ${NEORX_COMMAND}  
+case "${NEORX_COMMAND}" in
   run)  docker run --rm --name adminer -P 8080:8080 javanile/adminer
   stop) docker stop adminer
-  *)    echo "Undefined command, use: rum, stop."
+  *)    echo "Undefined command, use: run, stop."
 esac
