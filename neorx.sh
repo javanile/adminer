9ce83c881dc2ccd346760f37c2997fa7ddb2cad1
Francesco Bianco <info.francescobianco@gmail.com>
2019-12-13 18:41:22 +0100

    add readme.md

--- neorx.sh
+++ neorx.sh
@@ -6,7 +6,7 @@ export neorx_label=adminer
 ##
 
 case "${neorx_command}" in
-  run)  docker run --rm --name adminer -p 8080:8080 javanile/adminer ;;
+  run)  docker run --rm -d --name adminer -p 8080:8080 javanile/adminer ;;
   stop) docker stop adminer ;;
   *)    echo "Undefined command, use: run, stop." ;;
 esac
